0015767e38e7b2d8e21ad86b2d9a068c60650530
rafi-27 <147432818+rafi-27@users.noreply.github.com>
2023-11-02 14:57:51 +0100

    02/11

--- Java/U5/Parte 2/ejercicio_525.java
+++ Java/U5/Parte 2/ejercicio_525.java
@@ -1,35 +1,38 @@
 package unidad_5.segundaParte;
+
 import java.util.Scanner;
+
 public class ejercicio_525 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-/*Realiza un programa que lea y acepte únicamente aquellos que sean mayores que el
-último dado. La introducción de números finaliza con la introducción de un 0. Al final se mostrará:
-El total de números introducidos, excluido el 0. */
+        /*
+         * Realiza un programa que lea y acepte únicamente aquellos que sean mayores que
+         * el
+         * último dado. La introducción de números finaliza con la introducción de un 0.
+         * Al final se mostrará:
+         * El total de números introducidos, excluido el 0.
+         */
         int contador = 0;
         int numer = 0;
         int mayor = 0;
         int contadorFallido = 0;
-        System.out.print("Introduce un numero inicial(para salir 0 dos veces): ");
+
+        System.out.print("Introduce el numero inicial: ");
         numer = teclado.nextInt();
-        do {
-            System.out.println("Escribe un numero mayor: ");
+        while (numer != 0) {
+            System.out.print("Introduce un numero: ");
             mayor = teclado.nextInt();
 
-            if (numer > mayor){
-                System.out.println("Fallo es menor");
-                System.out.println("Introduce un numero inicial:");
-                numer = teclado.nextInt();
-                contadorFallido+=1;
-                contador+=1;
+            if (numer < mayor && numer != 0);
+            {
+            System.out.println("Error");
+            contadorFallido = contadorFallido + 1;
             }
 
-        contador=contador+1;
-        }while(numer != 0 && mayor != 0);
-        contadorFallido = contadorFallido-1;
-        contador = contador-1;
-        System.out.println("Total de numeros introducidos: "+contador);
-        System.out.println("Total de fallos: "+contadorFallido);
+            mayor = numer;
+            contador = contador + 1;
+
+        }
         teclado.close();
     }
 }
